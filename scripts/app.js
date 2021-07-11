ad197e9bbccfa1287bba88cc97fe54cc2c451ad2
navdeep <navdeepst.1987@gmail.com>
2021-07-11 16:55:49 +1000

    Calculations

--- scripts/app.js
+++ scripts/app.js
@@ -6,58 +6,110 @@
   RepaymentController.$inject = ['LoanRepaymentService'];
   function RepaymentController(LoanRepaymentService) { 
     var repayment = this;
-    repayment.amount = 0;
-    repayment.duration = 0; 
-    repayment.income = 0;
-    repayment.selectedName = "";
+    repayment.amount = 300000;
+    repayment.duration = 30; 
+    repayment.interest = 2.69;
+    repayment.monthlyPayment = 1215.21;
+    repayment.fortNightlyPayment = 567.09;
+    repayment.weeklyPayment = 567.09;
+    repayment.payment = repayment.monthlyPayment;
 
-    repayment.monthlyPayment = 0;
+    repayment.totalLoanPayment = 437475.68;
+    repayment.totalInterestCharged = 137475.68;
+    repayment.repaymentType = "Monthly";
+    repayment.monthlyPaymentColor = "orange";
+    repayment.fortNightlyPaymentColor = "white";
+    repayment.weeklyPaymentColor = "white";
 
-    repayment.banks = LoanRepaymentService.getBanks();
-   
-    repayment.calculateRepayment = function(bank, amount, duration) {
-      console.log(bank+" "+amount+" "+duration);
-      repayment.monthlyPayment = LoanRepaymentService.calculateLoanRepayment(bank, amount, duration);
-     
+    repayment.setRepaymentType = function(type) {
+      switch(type) {
+        case "Monthly":
+               repayment.monthlyPaymentColor = "orange";
+               repayment.fortNightlyPaymentColor = "white";
+               repayment.weeklyPaymentColor = "white";
+               repayment.repaymentType = "Monthly";
+               repayment.payment = repayment.monthlyPayment;
+               break;
+        case "FortNightly":
+               repayment.monthlyPaymentColor = "white";
+               repayment.fortNightlyPaymentColor = "orange";
+               repayment.weeklyPaymentColor = "white";
+               repayment.repaymentType = "FortNightly";
+               var monthlyPayment = repayment.monthlyPayment;
+               repayment.fortNightlyPayment = LoanRepaymentService.calculateFortNightlyPayment(monthlyPayment);
+               repayment.payment = repayment.fortNightlyPayment;
+               break;
+        case "Weekly":
+               repayment.monthlyPaymentColor = "white";
+               repayment.fortNightlyPaymentColor = "white";
+               repayment.weeklyPaymentColor = "orange";
+               repayment.repaymentType = "Weekly";
+               var monthlyPayment = repayment.monthlyPayment;
+               repayment.weeklyPayment = LoanRepaymentService.calculateWeeklyPayment(monthlyPayment);
+               repayment.payment = repayment.weeklyPayment;
+               break;
+
+      }
     }
+ 
+    repayment.calculateRepayment = function() {
+      var amount = repayment.amount;
+      var duration = repayment.duration;
+      var interest = repayment.interest;
+      
+      repayment.monthlyPayment = LoanRepaymentService.calculateLoanRepayment(amount, duration, interest);
+      repayment.payment = repayment.monthlyPayment;
+      var monthlyPayment = repayment.monthlyPayment;
+       //console.log(amount, duration, interest, monthlyPayment);
+      repayment.totalLoanPayment = LoanRepaymentService.calculateTotalLoanRepayment(duration, monthlyPayment);
+      //console.log(repayment.totalLoanPayment);
+      var totalLoanPayment = repayment.totalLoanPayment;
+       repayment.totalInterestCharged = LoanRepaymentService.calculateTotalInterestPayment(amount, totalLoanPayment);
+
+    }
+
+    
   }
 
   function LoanRepaymentService() {
     var service = this;
 
-    var banks = [
-        { 
-          name : "Commonwealth Bank", rate : 2.29, value : "cba"
-        },
-        {
-          name : "National Australian Bank", rate : 9.2, value : "nba"
-        },
-        {
-          name : "Westpac Bank", rate : 12.2, value : "wb"
-        },
-        {
-          name : "Macquarie Bank", rate : 10.4, value : "mb"
-        },
-        {
-           name : "Suncorp Bank", rate : 11.4, value : "sb"
-        }
-    ];
-
-    service.getBanks = function() {
-      return banks;
-    };
-
-    service.calculateLoanRepayment = function (bank, amount, duration) {
-      for(var i = 0; i < banks.length; ++i) {
-        if(banks[i].name === bank) {
+  
+    service.calculateLoanRepayment = function (amount, duration, interest) {
+      
          //  [P x R x (1+R)^N]/[(1+R)^N-1]
-          var monthlyRate = banks[i].rate / 1200;
+          var monthlyRate = interest / 1200;
 
           var result =  amount * monthlyRate * Math.pow((1+monthlyRate), (duration * 12)) / (Math.pow((1+monthlyRate), (duration * 12)) - 1);
           
-          return Math.round(result * 100) / 100;
-        }
-      }   
+          return service.toTwoDecimal(result);
+    };
+
+    service.calculateTotalLoanRepayment = function(duration, monthlyPayment) {
+          console.log("duration "+duration+" monthlyPayment "+monthlyPayment);
+          var result =  (duration * 12 * monthlyPayment);
+          return service.toTwoDecimal(result);
+    };
+
+    service.calculateTotalInterestPayment = function(amount, totalLoanPayment) {
+          var result =  totalLoanPayment - amount;
+           return service.toTwoDecimal(result);
+    };
+   
+    service.calculateFortNightlyPayment = function(monthlyPayment) {
+         var result = (monthlyPayment/30)*14;
+         console.log(result);
+          return service.toTwoDecimal(result);
+    };
+   
+     service.calculateWeeklyPayment = function(monthlyPayment) {
+         var result = (monthlyPayment/30)*7;
+         console.log(result);
+          return service.toTwoDecimal(result);
+    };
+    service.cal
+    service.toTwoDecimal = function(result) {
+      return Math.round(result * 100) / 100;
     };
   }
 
